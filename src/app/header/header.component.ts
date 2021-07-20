d8b14f0b335d18f6c89f0d599ad399d56a16a419
faquino <faquino@sms-latam.com>
2021-07-20 15:49:16 -0300

    adding functionality to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  private headerPics: string[] = [
+    'assets/header-bag-1.png',
+    'assets/header-bag-2.jpg',
+    'assets/header-bag-3.jpg',
+    'assets/header-bag-4.jpg'
+  ];
+  selectedPic: string;
+  private carouselCounter: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.carousel();
   }
 
+  carousel() {
+    console.log(this.headerPics.length);
+    if (this.carouselCounter == this.headerPics.length-1) this.carouselCounter = 0;
+
+    this.selectedPic = this.headerPics[this.carouselCounter];
+    this.carouselCounter++;
+
+    setTimeout(() => this.carousel(), 5000);
+  }
 }
